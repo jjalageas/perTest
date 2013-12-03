0d2bfce8246ca78f5a19203854085e396314ccf2
jjalageas <hotdonutdog@aol.com>
2013-12-03 12:37:04 +0100

    added source code

---
+++ src/perTest/TestClassOne.java
@@ -0,0 +1,20 @@
+package perTest;
+
+public class TestClassOne {
+	
+	private int alpha;
+	
+	public TestClassOne(){
+		alpha = 7;
+	}
+	
+	public int getAlpha(){
+		return alpha;
+	}
+	
+	public void setAlpha(int x){
+		alpha = x;
+	}
+	
+
+}
---
+++ src/perTest/TestClassTwo.java
@@ -0,0 +1,18 @@
+package perTest;
+
+public class TestClassTwo {
+	
+	private int beta;
+	
+	public TestClassTwo(){
+		beta = 7;
+	}
+	
+	public int getBeta(){
+		return beta;
+	}
+	
+	public void setBeta(int x){
+		beta = x;
+	}
+}
